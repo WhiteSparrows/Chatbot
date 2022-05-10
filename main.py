c3680a0a8de7845a501e5206f0aa04b418e254b5
Nicolas Cavaillès--Wurmser <nicolas.cavailles--wurmser@grenoble-inp.org>
2022-05-10 15:23:22 +0200

    training done

--- main.py
+++ main.py
@@ -78,3 +78,19 @@ training = np.array(training)
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 print (len(words), "unique stemmed words", words)
+
+
+# reset underlying graph data
+tf.compat.v1.reset_default_graph()
+# Build neural network
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Define model and setup tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+# Start training (apply gradient descent algorithm)
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('model.tflearn')
